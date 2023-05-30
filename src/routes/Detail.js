673adb5a260e61200e6df04f64a159b5570ef602
Park ChangHyub <thwlckd@naver.com>
2023-05-31 01:16:03 +0900

    뒤로가기 버튼 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import styles from "./Detail.module.css";
+import { useNavigate } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
@@ -16,7 +17,13 @@ function Detail() {
   useEffect(() => {
     getMovie();
   }, []);
-  console.log(movie);
+  // console.log(movie);
+
+  // 뒤로가기 버튼
+  const navigate = useNavigate(); 
+  const onClickBtn = () => {
+    navigate(-1);
+  };
   return (
     <div className={styles.container}>
       {loading ? (
@@ -24,27 +31,32 @@ function Detail() {
           <span>Loading...</span>
         </div>
       ) : (
-        <div className={styles.movie}>
-          <img
-            src={movie.medium_cover_image}
-            alt={movie.title}
-            className={styles.movie__img}
-          />
-          <div className={styles.movie__text}>
-            <Link to={`${movie.url}`}>
-              <h2 className={styles.movie__title}>{movie.title}</h2>
-            </Link>
-            <h3 className={styles.movie__year}>{movie.year}</h3>
-            <ul className={styles.movie__genres}>
-              {movie.genres.map((g) => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-            <p>
-              {movie.description_full.length > 235
-                ? `${movie.description_full.slice(0, 235)}...`
-                : movie.description_full}
-            </p>
+        <div>
+          <header>
+            <button className={styles.button__before} onClick={onClickBtn}>←</button>
+          </header>
+          <div className={styles.movie}>
+            <img
+              src={movie.medium_cover_image}
+              alt={movie.title}
+              className={styles.movie__img}
+            />
+            <div className={styles.movie__text}>
+              <Link to={`${movie.url}`}>
+                <h2 className={styles.movie__title}>{movie.title}</h2>
+              </Link>
+              <h3 className={styles.movie__year}>{movie.year}</h3>
+              <ul className={styles.movie__genres}>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+              <p>
+                {movie.description_full.length > 235
+                  ? `${movie.description_full.slice(0, 235)}...`
+                  : movie.description_full}
+              </p>
+            </div>
           </div>
         </div>
       )}
