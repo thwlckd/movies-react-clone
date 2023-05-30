792749bff09834ef83387114c29c56a8d15b8c15
Park ChangHyub <thwlckd@naver.com>
2023-05-31 01:56:57 +0900

    Route path 수정

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />}></Route>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
