b69ec21944fcdb16e05df5f8237d01f49767a30d
Park ChangHyub <thwlckd@naver.com>
2023-05-31 00:32:53 +0900

    메인 UI 변경 / 영화 상세 페이지 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,55 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  console.log(movie);
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className={styles.movie}>
+          <img
+            src={movie.medium_cover_image}
+            alt={movie.title}
+            className={styles.movie__img}
+          />
+          <div className={styles.movie__text}>
+            <Link to={`${movie.url}`}>
+              <h2 className={styles.movie__title}>{movie.title}</h2>
+            </Link>
+            <h3 className={styles.movie__year}>{movie.year}</h3>
+            <ul className={styles.movie__genres}>
+              {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+            <p>
+              {movie.description_full.length > 235
+                ? `${movie.description_full.slice(0, 235)}...`
+                : movie.description_full}
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
